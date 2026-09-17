6e5adeda692f2ce36de2d36f1a648d5600ea6dd7
Christian Garlen <chadgarval@outlook.com>
2026-09-17 16:07:29 -0400

    feat: implement core application structure with routing, layout components, and context providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { useState } from 'react'
+import { Layout } from "./Layout/Layout"
+import { HashRouter, Routes, Route } from "react-router-dom"
+import { Home } from "./Layout/Body/Home"
+import { Category } from "./Layout/Body/Category"
+import { Recipe } from "./Layout/Body/Recipe"
+import { Favorites } from "./Layout/Body/Favorites"
+import { Search } from "./Layout/Body/Search"
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <section data-theme={'light'} className='min-h-screen w-full'>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} />
+            <Route path="/category/:categoryName" element={<Category />} />
+            <Route path="/recipe/:recipeId" element={<Recipe />} />
+            <Route path="/favorites" element={<Favorites />} />
+            <Route path="/search/:query" element={<Search />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </section>
   )
 }
 
---
+++ src/Layout/Header/NavBar.tsx
@@ -0,0 +1,49 @@
+import { SearchBar } from "../../Components/NavBar/SearchBar"
+
+export default function NavBar() {
+    const item1 = <SearchBar />
+    return (
+        <>
+            <div className="drawer">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle lg:hidden" />
+                <div className="drawer-content flex flex-col">
+                    {/* Navbar */}
+                    <div className="navbar bg-base-300 w-full">
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-2" aria-label="open sidebar" className="btn btn-square btn-ghost drawer-button">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block h-6 w-6 stroke-current"
+                        >
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 6h16M4 12h16M4 18h16"
+                            ></path>
+                        </svg>
+                        </label>
+                    </div>
+                    <div className="mx-2 flex-1 px-2">Navbar Title</div>
+                    <div className="hidden flex-none lg:block">
+                        <ul className="menu menu-horizontal">
+                        {/* Navbar menu content here */}
+                        <li>{item1}</li>
+                        </ul>
+                    </div>
+                    </div>
+                    {/* Page content here */}
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                    {/* Sidebar content here */}
+                        <li>{item1}</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
